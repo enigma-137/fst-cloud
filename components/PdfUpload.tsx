da814f90ce47d9b2312e8f3331bd2b6dff34eee3
enigma-137 <emmanuelonoja291@gmail.com>
2025-02-05 16:52:10 +0100

    [fix again]

--- components/PdfUpload.tsx
+++ components/PdfUpload.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 import { createClient } from "@/lib/supabase";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
